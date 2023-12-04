8130fb1a53731f13a35b944572b450e983b240b8
gianluca cattin <gcattin97@gmail.com>
2023-12-04 23:59:32 +0100

    DONE (i think)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState } from 'react';
 import PDFUploader from './components/PDFUploader';
 import PDFViewer from './components/PDFViewer';
 import { PDFDocument, StandardFonts, rgb } from 'pdf-lib';
@@ -14,52 +14,42 @@ const App = () => {
         const fileReader = new FileReader();
         fileReader.onload = async (event) => {
             const pdfBytes = new Uint8Array(event.target.result);
-            setOriginalPdfBytes(pdfBytes); // Save the original PDF bytes
+            setOriginalPdfBytes(pdfBytes);
             setPdfBlob(new Blob([pdfBytes], { type: "application/pdf" }));
         };
         fileReader.readAsArrayBuffer(file);
     };
 
     const signDocument = async (pdfBlob) => {
-      if (typeof window.ethereum === 'undefined') {
-          alert('MetaMask is not installed.');
-          return;
-      }
+        if (typeof window.ethereum === 'undefined') {
+            alert('MetaMask is not installed.');
+            return;
+        }
 
-      const web3 = new Web3(window.ethereum);
+        const web3 = new Web3(window.ethereum);
 
-      try {
-          await window.ethereum.enable();
-          const accounts = await web3.eth.getAccounts();
-          const account = accounts[0];
-          console.log('User account:', account);
+        try {
+            await window.ethereum.enable();
+            const accounts = await web3.eth.getAccounts();
+            const account = accounts[0];
 
-          const pdfData = await new Response(pdfBlob).arrayBuffer();
-          const pdfBytesHex = '0x' + [...new Uint8Array(pdfData)]
-                  .map((byte) => byte.toString(16).padStart(2, '0'))
-                  .join('');
-          const pdfHash = web3.utils.sha3(pdfBytesHex);
-          console.log('PDF Hash:', pdfHash);
+            const pdfData = await new Response(pdfBlob).arrayBuffer();
+            const pdfBytesHex = '0x' + [...new Uint8Array(pdfData)]
+                .map((byte) => byte.toString(16).padStart(2, '0'))
+                .join('');
+            const pdfHash = web3.utils.sha3(pdfBytesHex);
 
-          // Passing `null` or an empty string as the third argument
-          const signature = await web3.eth.personal.sign(pdfHash, account, '');
-
-          return {
-              signer: account,
-              signature,
-              timestamp: Math.floor(Date.now() / 1000)
-          };
-      } catch (error) {
-          console.error('Error signing document:', error);
-      }
-  };
-
-
-
-
-    const handleStamp = useCallback((stampData) => {
-        setStamps(currentStamps => [...currentStamps, stampData]);
-    }, []);
+            const signature = await web3.eth.personal.sign(pdfHash, account, '');
+            return {
+                signer: account,
+                signature,
+                timestamp: Math.floor(Date.now() / 1000),
+                pdfHash: pdfHash
+            };
+        } catch (error) {
+            console.error('Error signing document:', error);
+        }
+    };
 
     const downloadStampedPDF = async () => {
         const pdfDoc = await PDFDocument.load(originalPdfBytes);
@@ -67,12 +57,19 @@ const App = () => {
 
         stamps.forEach(async (stamp) => {
             const page = pdfDoc.getPages()[stamp.page - 1];
-            page.drawText(`${stamp.text}\n${stamp.signer}\n${stamp.signature}\nTimestamp: ${stamp.timestamp}`, {
-                x: stamp.x,
-                y: page.getHeight() - stamp.y,
-                size: 10,
-                font: helveticaFont,
-                color: rgb(0.95, 0.1, 0.1),
+            const lines = stamp.text.split('\n');
+            let currentY = stamp.y;
+
+            lines.forEach((line) => {
+
+                page.drawText(line, {
+                    x: stamp.x,
+                    y: page.getHeight() - currentY,
+                    size: 10,
+                    font: helveticaFont,
+                });
+
+                currentY += 12;
             });
         });
 
@@ -86,19 +83,19 @@ const App = () => {
     };
 
     return (
-      <div className="container">
-          {!pdfBlob ? (
-              <PDFUploader onFileSelect={handleFileSelect} />
-          ) : (
-              <>
-                  <PDFViewer pdfBlob={pdfBlob} onStamp={handleStamp} signDocument={signDocument} />
-                  <button className="button" onClick={downloadStampedPDF}>
-                      Download Stamped PDF
-                  </button>
-              </>
-          )}
-      </div>
-  );
+        <div className="container">
+            {!pdfBlob ? (
+                <PDFUploader onFileSelect={handleFileSelect} />
+            ) : (
+                <>
+                    <PDFViewer pdfBlob={pdfBlob} onStamp={setStamps} signDocument={signDocument} stamps={stamps} />
+                    <button className="button" onClick={downloadStampedPDF}>
+                        Download Stamped PDF
+                    </button>
+                </>
+            )}
+        </div>
+    );
 };
 
 export default App;
--- src/components/PDFViewer.js
+++ src/components/PDFViewer.js
@@ -1,76 +1,91 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import * as pdfjsLib from 'pdfjs-dist/build/pdf';
 import pdfjsWorker from 'pdfjs-dist/build/pdf.worker.entry';
 
 pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 
-const PDFViewer = ({ pdfBlob, onStamp, signDocument }) => {
-  const containerRef = useRef(null);
-  const [stamps, setStamps] = useState([]);
+const PDFViewer = ({ pdfBlob, signDocument, stamps, onStamp }) => {
+    const containerRef = useRef(null);
 
-  const handleCanvasClick = async (e, canvas, pageNumber) => {
-    const context = canvas.getContext('2d');
-    const x = e.offsetX;
-    const y = e.offsetY;
-
-    try {
-      const signatureData = await signDocument(pdfBlob);
-      if (signatureData) {
-        const stampText = `Signed by: ${signatureData.signer}\nSignature: ${signatureData.signature}\nTimestamp: ${signatureData.timestamp}`;
-        const newStamp = { text: stampText, x, y, page: pageNumber };
-        setStamps(currentStamps => [...currentStamps, newStamp]);
-        context.fillText(stampText, x, y);
-
-      } else {
-        console.error('Signature data is undefined');
+    const chunkSignature = (signature, label, chunkSize) => {
+      const labelLength = label.length;
+      const chunks = [];
+      for (let i = 0; i < signature.length; i += chunkSize) {
+          const chunk = signature.substring(i, Math.min(i + chunkSize, signature.length));
+          if (i === 0) {
+              chunks.push(`${label}${chunk}`);
+          } else {
+              chunks.push(chunk);
+          }
       }
-    } catch (error) {
-      console.error('Error in handleCanvasClick:', error);
-    }
+      return chunks.join('\n');
   };
 
-  const redrawStamps = (canvas, pageNumber, context) => {
-    const pageStamps = stamps.filter(stamp => stamp.page === pageNumber);
-    pageStamps.forEach(stamp => {
-      context.fillText(stamp.text, stamp.x, stamp.y);
-    });
-  };
+    const handleCanvasClick = async (e, canvas, pageNumber) => {
+        const rect = canvas.getBoundingClientRect();
+        const scale = canvas.width / rect.width;
+        const x = (e.clientX - rect.left) * scale;
+        const y = (e.clientY - rect.top) * scale;
 
-  useEffect(() => {
-    const renderPageOnCanvas = async (pdf, pageNumber, container) => {
-      const page = await pdf.getPage(pageNumber);
-      const viewport = page.getViewport({ scale: 1.2 });
-      const canvas = document.createElement('canvas');
-      canvas.height = viewport.height;
-      canvas.width = viewport.width;
-      const context = canvas.getContext('2d');
-
-      container.appendChild(canvas);
-
-      await page.render({ canvasContext: context, viewport }).promise;
-      redrawStamps(canvas, pageNumber, context);
-
-      canvas.addEventListener('click', (e) => handleCanvasClick(e, canvas, pageNumber));
+        try {
+          const signatureData = await signDocument(pdfBlob);
+          if (signatureData) {
+              const label = "signature: "; // Your label
+              const wrappedSignature = chunkSignature(signatureData.signature, label, 71); // Use the desired chunk size
+              const stampText = `address: ${signatureData.signer}\nhash: ${signatureData.pdfHash}\n${wrappedSignature}\nTimestamp: ${signatureData.timestamp}`;
+              onStamp([...stamps, { text: stampText, x, y, page: pageNumber }]);
+          } else {
+              console.error('Signature data is undefined');
+          }
+        } catch (error) {
+            console.error('Error in handleCanvasClick:', error);
+        }
     };
 
-    if (pdfBlob) {
-      const loadingTask = pdfjsLib.getDocument(URL.createObjectURL(pdfBlob));
-      loadingTask.promise.then(async (pdf) => {
-        const container = containerRef.current;
-        container.innerHTML = '';
+    useEffect(() => {
+        const renderPageOnCanvas = async (pdf, pageNum) => {
+            const page = await pdf.getPage(pageNum);
+            const viewport = page.getViewport({ scale: 1.5 });
+            const canvas = document.createElement('canvas');
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
+            const context = canvas.getContext('2d');
+            const renderContext = { canvasContext: context, viewport: viewport };
 
-        for (let pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
-          await renderPageOnCanvas(pdf, pageNum, container);
+            // Append the new canvas for each page
+            containerRef.current.appendChild(canvas);
+            await page.render(renderContext).promise;
+
+            // Draw the stamps for the current page
+            stamps.filter(stamp => stamp.page === pageNum).forEach(stamp => {
+                const lines = stamp.text.split('\n');
+                let currentY = stamp.y;
+                lines.forEach(line => {
+                    context.fillText(line, stamp.x, currentY);
+                    currentY += 10; // Adjust for line height
+                });
+            });
+
+            canvas.addEventListener('click', (e) => handleCanvasClick(e, canvas, pageNum));
+        };
+
+        if (pdfBlob) {
+            const loadingTask = pdfjsLib.getDocument(URL.createObjectURL(pdfBlob));
+            loadingTask.promise.then(async (pdf) => {
+                // Clear existing content
+                while (containerRef.current.firstChild) {
+                    containerRef.current.removeChild(containerRef.current.firstChild);
+                }
+
+                // Render each page
+                for (let pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
+                    await renderPageOnCanvas(pdf, pageNum);
+                }
+            });
         }
-      });
-    }
-  }, [pdfBlob, stamps]); // re-render when pdfBlob or stamps change
+    }, [pdfBlob, signDocument, stamps, onStamp]);
 
-  return (
-    <div ref={containerRef} className="pdf-container">
-      {/* Canvases will be appended here */}
-    </div>
-  );
+    return <div ref={containerRef} className="pdf-container" style={{ position: 'relative' }}></div>;
 };
 
 export default PDFViewer;
